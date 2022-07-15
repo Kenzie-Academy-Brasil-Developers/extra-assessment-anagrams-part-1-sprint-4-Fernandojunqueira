5364b6dac6081067b9071e007da97921a55395fb
Fernandojunqueira <fernandojunqueira12@gmail.com>
2022-07-15 14:50:52 -0300

    Entrega Sexta

--- anagrams1/anagrams1.js
+++ anagrams1/anagrams1.js
@@ -2,19 +2,39 @@ const button = document.getElementById("findButton");
     button.addEventListener("click", function () {
 
     let typedText = document.getElementById("input").value;
-    console.log(typedText)
-    getAnagramsOf(typedText);
+    let paragraph = document.querySelector('p')
+    paragraph.innerText = `[${getAnagramsOf(typedText)}]`;
+    
 
-    console.log('oi')
 });
 
-function getAnagramsOf(palavras){
 
-    for(let i = 0; i < palavras.length; i++ ){
+function getAnagramsOf(word){
 
-        let teste = palavras[i]
-    }
-};
+   let b = alphabetize(word)
+   return listarPalavras(b)
+   
+  
+}
+
+function listarPalavras(arr){
+
+    let array = []
+    for( let i = 0; i < palavras.length; i++){
+
+        if(arr === palavras[i].toLowerCase().split("").sort().join("").trim()) {
+
+            array.push(palavras[i])
+        }  
+        
+}
+
+return array
+}
+
+
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
+listarPalavras(palavras)
+
